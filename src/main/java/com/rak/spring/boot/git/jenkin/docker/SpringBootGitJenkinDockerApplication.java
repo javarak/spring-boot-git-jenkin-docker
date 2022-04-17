de2f736d050f7448fb5d23a24d922aa022da1f6f
Rakesh Kumar <rakesh@siemens.com>
2022-04-17 13:52:20 +0530

    added change

--- src/main/java/com/rak/spring/boot/git/jenkin/docker/SpringBootGitJenkinDockerApplication.java
+++ src/main/java/com/rak/spring/boot/git/jenkin/docker/SpringBootGitJenkinDockerApplication.java
@@ -12,7 +12,7 @@ public class SpringBootGitJenkinDockerApplication {
 	@GetMapping("/message")
 	public String getMessage() {
 
-		return "Welcome to Spring Docker by Git Jenkin Pipeline..!!";
+		return "Welcome to Spring Docker by Git Jenkin Pipeline change..!!";
 	}
 
 	public static void main(String[] args) {
